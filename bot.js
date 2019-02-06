59879533cd6a5934c2ef31511eef67e913ed1432
Fras#9999 <44925264+frasgamer@users.noreply.github.com>
2019-02-06 20:31:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3119,20 +3119,20 @@ client.on('message', message => {//FRAS
   var argresult = message.content.split(` `).slice(1).join(' ');//FRAS
     if (!devs.includes(message.author.id)) return;//FRAS
     
-if (message.content.startsWith(adminprefix + 'play')) {//fras
+if (message.content.startsWith(adminprefix + 'setp')) {//fras
   client.user.setGame(argresult);//fras
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)//fras
 } else //fras
-  if (message.content.startsWith(adminprefix + 'name')) {//fras
+  if (message.content.startsWith(adminprefix + 'setn')) {//fras
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'avatar')) {
+  if (message.content.startsWith(adminprefix + 'seta')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
-if (message.content.startsWith(adminprefix + 'streaming')) {
+if (message.content.startsWith(adminprefix + 'sets')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");//wennnn
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
